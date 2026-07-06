9e59177c090258b7fbe9c2b439250b2c6feabcb7
Gregory Laurent <gregory.m.laurent@gmail.com>
2026-07-06 13:36:50 -0700

    fix an issue with new line of zsh in emacs

--- modules/home/zsh.nix
+++ modules/home/zsh.nix
@@ -20,8 +20,15 @@
       nix-rbs = "sudo nixos-rebuild switch --flake ~/.config/nixos#$(hostname)";
       vim = "neovim";
       ec = "emacsclient -c -a ''";
-      et = "emacsclient -t -a ''"; 
+      et = "emacsclient -t -a ''";
     };
+
+    # Emacs' vterm/term mis-detects partial lines and shows zsh's end-of-line
+    # marker (a reverse-video '%') on every prompt. Blank it inside Emacs only,
+    # so real terminals keep the (occasionally useful) marker.
+    initContent = ''
+      [[ -n "$INSIDE_EMACS" ]] && export PROMPT_EOL_MARK=""
+    '';
   };
 
   programs.starship.enable = true;
